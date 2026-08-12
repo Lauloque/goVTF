1206ca3f54b443a2181688e46b737dd2796de8bb
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-08-12 22:07:58 +0200

    placeholder for future vmt gen

---
+++ vmt/writer.go
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+package vmt
+
+// Reference:
+//
+//
+// "UnlitGeneric"
+// {
+// 	"$basetexture"	"vgui/logos/test_512"
+// 	"$translucent" "1"
+// 	"$ignorez" "1"
+// 	"$vertexcolor" "1"
+// 	"$vertexalpha" "1"
+// }
