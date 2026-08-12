8cdb16e6c4a41b4cfa8956ce57f069f0fe3aa2bf
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-08-12 16:42:26 +0200

    Create constants.go

---
+++ vtf/constants.go
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+package vtf
+
+// Based on https://developer.valvesoftware.com/wiki/VTF_(Valve_Texture_Format)
+
+// File signatures and format codes
+const (
+	SignatureVersionMajor = 7
+	SignatureVersionMinor = 4
+	HeaderSize            = 96 // 80 base + 16 for 7.3+ fields
+
+	ImageFormatRGBA8888 = 0
+	ImageFormatDXT1     = 14
+)
+
+// Resource tags (cannot be constants in Go)
+var (
+	TagHIRES = [3]byte{0x30, 0x00, 0x00} // High-res image data
+	TagLORES = [3]byte{0x01, 0x00, 0x00} // Low-res thumbnail
+)
+
+// Helper to create resource tag from string (for extension purposes)
+func MakeTag(tag string) [3]byte {
+	b := [3]byte{}
+	copy(b[:], tag)
+	return b
+}
