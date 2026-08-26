e816e3f8dbad20d0a02a597efc5a064e13d0f2e7
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-08-26 14:33:05 +0200

    update test file

--- compare_vtf_files.sh
+++ compare_vtf_files.sh
@@ -2,7 +2,7 @@
 
 # Compare first 128 bytes (header + some data)
 diff <(hexdump -C out/test_512.vtf | head -n 8) \
-     <(hexdump -C controlTest/controlTest_512_RGBA8888.vtf | head -n 8)
+     <(hexdump -C controlTest/controlTest_512_DXT1-slowQ.vtf | head -n 8)
 
 # Check specific fields
 echo "=== Our file ==="
@@ -12,7 +12,7 @@ hexdump -C out/test_512.vtf -s 61 -n 2  # LowRes dimensions
 hexdump -C out/test_512.vtf -s 68 -n 4  # NumResources
 
 echo "=== Control file ==="
-hexdump -C controlTest/controlTest_512_RGBA8888.vtf -s 52 -n 4
-hexdump -C controlTest/controlTest_512_RGBA8888.vtf -s 56 -n 1
-hexdump -C controlTest/controlTest_512_RGBA8888.vtf -s 61 -n 2
-hexdump -C controlTest/controlTest_512_RGBA8888.vtf -s 68 -n 4
+hexdump -C controlTest/controlTest_512_DXT1-slowQ.vtf -s 52 -n 4
+hexdump -C controlTest/controlTest_512_DXT1-slowQ.vtf -s 56 -n 1
+hexdump -C controlTest/controlTest_512_DXT1-slowQ.vtf -s 61 -n 2
+hexdump -C controlTest/controlTest_512_DXT1-slowQ.vtf -s 68 -n 4
